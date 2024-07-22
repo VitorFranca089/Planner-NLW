0930b972d0ecbc0a8216abdf2242a4064a990c24
Vitor Franca <vitorfranca089@gmail.com>
2024-07-22 13:08:04 -0300

    feat: add Swagger configurations

--- src/main/java/com/rocketseat/planner/participant/ParticipantController.java
+++ src/main/java/com/rocketseat/planner/participant/ParticipantController.java
@@ -1,5 +1,6 @@
 package com.rocketseat.planner.participant;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +14,7 @@ public class ParticipantController {
     @Autowired
     private ParticipantService participantService;
 
+    @Operation(summary = "Confirma participação na viagem", description = "Metodo que o participante confirma sua presença na viagem", tags = "Participante")
     @PostMapping("/{id}/confirm")
     public ResponseEntity<ParticipantResponseConfirm> confirmParticipant(@PathVariable UUID id, @RequestBody ParticipantRequestPayload payload){
         ParticipantResponseConfirm participantResponseConfirm = this.participantService.confirmParticipant(id, payload);
--- src/main/java/com/rocketseat/planner/trip/TripController.java
+++ src/main/java/com/rocketseat/planner/trip/TripController.java
@@ -9,6 +9,7 @@ import com.rocketseat.planner.link.LinkResponse;
 import com.rocketseat.planner.participant.ParticipantCreateResponse;
 import com.rocketseat.planner.participant.ParticipantData;
 import com.rocketseat.planner.participant.ParticipantRequestPayload;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -27,12 +28,14 @@ public class TripController {
     // Criação do TripService para separar o core das trips - OK
 
     // Trips
+    @Operation(summary = "Cria uma viagem", description = "Método que cria uma nova viagem", tags = "Viagem")
     @PostMapping
     public ResponseEntity<TripCreateResponse> createTrip(@RequestBody @Valid TripRequestPayload payload){
         TripCreateResponse tripCreateResponse = this.tripService.createTrip(payload);
         return ResponseEntity.ok(tripCreateResponse);
     }
 
+    @Operation(summary = "Obtém detalhes de uma viagem", description = "Método que obtém detalhes de uma viagem a partir do id da viagem", tags = "Viagem")
     @GetMapping("/{id}")
     public ResponseEntity<TripData> getTripDetails(@PathVariable UUID id){
         TripData tripData = this.tripService.getTripDetails(id);
@@ -42,7 +45,7 @@ public class TripController {
         return ResponseEntity.notFound().build();
     }
 
-
+    @Operation(summary = "Altera dados de uma viagem", description = "Método que altera os dados de destino, início e fim de uma viagem", tags = "Viagem")
     @PutMapping("/{id}")
     public ResponseEntity<TripData> updateTrip(@PathVariable UUID id, @RequestBody @Valid TripRequestPayload payload){
         TripData tripData = this.tripService.updateTrip(id, payload);
@@ -52,6 +55,7 @@ public class TripController {
         return ResponseEntity.notFound().build();
     }
 
+    @Operation(summary = "Confirma viagem", description = "Método que confirma que a viagem será realizada", tags = "Viagem")
     @GetMapping("/{id}/confirm")
     public ResponseEntity<TripData> confirmTrip(@PathVariable UUID id){
         TripData tripData = this.tripService.confirmTrip(id);
@@ -62,6 +66,7 @@ public class TripController {
     }
 
     // Activities
+    @Operation(summary = "Cria uma atividade", description = "Método que cria uma atividade para a viagem", tags = "Atividade")
     @PostMapping("/{id}/activities")
     public ResponseEntity<ActivityResponse> registerActivity(@PathVariable UUID id, @RequestBody ActivityRequestPayload payload){
         ActivityResponse activityResponse = this.tripService.registerActivity(id, payload);
@@ -71,6 +76,7 @@ public class TripController {
         return ResponseEntity.notFound().build();
     }
 
+    @Operation(summary = "Obtém todas as atividades da viagem", description = "Método que obtém todas as atividaes da viagem a partir do id da viagem", tags = "Atividade")
     @GetMapping("/{id}/activities")
     public ResponseEntity<List<ActivityData>> getAllActivities(@PathVariable UUID id){
         List<ActivityData> activityDataList = this.tripService.getAllActivities(id);
@@ -78,6 +84,7 @@ public class TripController {
     }
 
     // Participants
+    @Operation(summary = "Registra um participante na viagem", description = "Método que registra um participante na viagem", tags = "Participante")
     @PostMapping("/{id}")
     public ResponseEntity<ParticipantCreateResponse> registerParticipant(@PathVariable UUID id, @RequestBody ParticipantRequestPayload payload){
         ParticipantCreateResponse participantCreateResponse = this.tripService.registerParticipant(id, payload);
@@ -87,6 +94,7 @@ public class TripController {
         return ResponseEntity.notFound().build();
     }
 
+    @Operation(summary = "Convida um participante para viagem", description = "Método que convida um participante para a viagem", tags = "Participante")
     @PostMapping("/{id}/invite")
     public ResponseEntity<ParticipantCreateResponse> inviteParticipant(@PathVariable UUID id, @RequestBody ParticipantRequestPayload payload){
         ParticipantCreateResponse participantCreateResponse = this.tripService.inviteParticipant(id, payload);
@@ -96,6 +104,7 @@ public class TripController {
         return ResponseEntity.notFound().build();
     }
 
+    @Operation(summary = "Obtém todos os participantes da viagem", description = "Método que obtém todos os participantes da viagem a partir do id da viagem", tags = "Participante")
     @GetMapping("/{id}/participants")
     public ResponseEntity<List<ParticipantData>> getAllParticipants(@PathVariable UUID id){
         List<ParticipantData> participantDataList = this.tripService.getAllParticipants(id);
@@ -103,6 +112,7 @@ public class TripController {
     }
 
     // Links
+    @Operation(summary = "Registra um link para viagem", description = "Método que registra um link para viagem", tags = "Link")
     @PostMapping("/{id}/links")
     public ResponseEntity<LinkResponse> registerLink(@PathVariable UUID id, @RequestBody LinkRequestPayload payload){
         LinkResponse linkResponse = this.tripService.registerLink(id, payload);
@@ -113,6 +123,7 @@ public class TripController {
         }
     }
 
+    @Operation(summary = "Obtém os links da viagem", description = "Método que obtém todos os links da viagem a partir do id da viagem", tags = "Link")
     @GetMapping("/{id}/links")
     public ResponseEntity<List<LinkData>> getAllLinks(@PathVariable UUID id){
             List<LinkData> linkData = this.tripService.getAllLinks(id);
